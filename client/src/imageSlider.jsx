02626e97e33b570dc00e1b65abc731ef6fb20f7d
Harshjain09012004 <harshjain6812@gmail.com>
2024-09-17 09:27:08 +0530

    Slider

--- client/src/imageSlider.jsx
+++ client/src/imageSlider.jsx
@@ -4,21 +4,21 @@ import { IoArrowBackCircleOutline } from "react-icons/io5";
 
 const Slider = () => {
     const [loc, setloc] = useState(0);
-    let imageArray = ['src/assets/MkGandhi.jpg','src/assets/Swami.jpg','src/assets/BhagatSingh.jpg'];
+    let imageArray = ['src/assets/Image1.avif','src/assets/Image7.jpg','src/assets/Image3.webp','src/assets/Image5.jpg','src/assets/Image2.jpg','src/assets/Image6.jpg','src/assets/Image4.jpg'];
 
     return (
-        <div className='bg-zinc-100 p-4 border border-slate-900 mt-10'>
+        <div className='bg-zinc-100 p-4 border border-b-slate-300 mt-10'>
             <p className='text-2xl text-center font-semibold m-6'>Our Photo Gallery</p>
 
-            <div className='flex justify-evenly place-items-center'>
+            <div className='flex justify-evenly place-items-center mb-10'>
                 <IoArrowBackCircleOutline className='text-3xl active:scale-110 transition-all' onClick={()=>{
-                    setloc((loc + 1)%3);
+                    setloc((loc + 1)%7);
                 }}/>
-
-                <img width={'230px'} src={imageArray[loc]} className='transition-all'/>
+                
+                <img src={imageArray[loc]} className='w-[750px] h-[460px] rounded-xl shadow-gray-500 shadow-lg transition-all object-cover'/>
 
                 <IoArrowForwardCircleOutline className='text-3xl active:scale-110 transition-all' onClick={()=>{
-                    setloc((loc - 1 + 3)%3);
+                    setloc((loc - 1 + 7)%7);
                 }}/>
             </div>
         </div>
