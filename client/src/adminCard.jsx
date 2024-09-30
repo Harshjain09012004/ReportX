062ca45d9b90ba11b8d8d6714b87890076a89406
Harshjain09012004 <harshjain6812@gmail.com>
2024-09-30 11:33:01 +0530

    Added Extra Layer For Deletion

--- client/src/adminCard.jsx
+++ client/src/adminCard.jsx
@@ -1,16 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import axios from 'axios';
 import { HiDotsVertical } from "react-icons/hi";
 import { RxUpdate } from "react-icons/rx";
 import { RiDeleteBinLine } from "react-icons/ri";
+import { usercontext } from './UserContext';
 
 const AdminCard = ({det}) => {
 
+  const {user} = useContext(usercontext);
   const [statusClass, setstatusClass] = useState();
   const [menuclass, setmenuclass] = useState('hidden');
   const [update,setupdate] = useState(false);
   const [newStatus,setnewStatus] = useState(det.status);
   const [tempState, settempState] = useState(det.status);
+  const [doAsk, setdoAsk] = useState(false);
+  const [cnfmName, setcnfmName] = useState('');
 
   useEffect(() => {
     if(newStatus == 'Pending') setstatusClass('text-lg  font-medium' + ' text-orange-400');
@@ -36,17 +40,30 @@ const AdminCard = ({det}) => {
     catch{alert("Service Unavailable");}
   }
 
+  function confirmDelete(){
+    if(cnfmName == user){
+      deleteComplaint(); setdoAsk(false);
+    }
+    else{
+      alert('Complaint Cannot Be Deleted');
+      setcnfmName('');
+    }
+  }
+
   async function deleteComplaint(){
     try{
       const res = await axios.post('/deleteComplaint',{"id":det._id,"regMail":det.registrarMail});;
       if(!res.data.Success) alert("Something Wrong");
-      setnewStatus('Terminated'); det.status = 'Terminated';
+
+      setnewStatus('Terminated'); 
+      setmenuclass('hidden');
+      det.status = 'Terminated';
     }
     catch{alert("Unable To Process");}
   }
 
   return (
-    <div className='Card m-8 ml-16 border w-[80%] border-gray-300 rounded-2xl shadow-neutral-400 shadow-lg cursor-pointer transition-all hover:shadow-zinc-500 hover:shadow-md'>
+    <div className='Card relative m-8 ml-16 border w-[80%] border-gray-300 rounded-2xl shadow-neutral-400 shadow-lg cursor-pointer transition-all hover:shadow-zinc-500 hover:shadow-md'>
 
         <div className='flex flex-row gap-16 place-items-center w-[100%] h-96 relative
         p-1 '>
@@ -56,7 +73,9 @@ const AdminCard = ({det}) => {
           <div className={`absolute bg-zinc-200 top-[70px] right-6 w-32 text-md rounded-2xl transition-all z-20 shadow-md shadow-zinc-400 ${menuclass}`}>
         
             <div className='flex flex-col gap-2 mt-4 mb-4'>
-                <div className='flex gap-3 justify-center place-items-center text-center p-1 hover:bg-slate-400 transition-colors' onClick={deleteComplaint}>
+                <div className='flex gap-3 justify-center place-items-center text-center p-1 hover:bg-slate-400 transition-colors' onClick={()=>{
+                  setdoAsk(!doAsk); setmenuclass('hidden');
+                }}>
                   <p>Delete</p>
                   <RiDeleteBinLine/>
                 </div>
@@ -153,6 +172,33 @@ const AdminCard = ({det}) => {
         
           </div>
         </div>
+        
+
+        {doAsk && (
+          <div className='h-[100%] w-[100%] absolute top-0 left-0 rounded-2xl flex justify-center place-items-center backdrop-blur-sm transition-all'>
+        
+            <div className='flex flex-col gap-8 place-items-center bg-white p-8 px-10 rounded-3xl border border-gray-300 shadow-lg shadow-gray-400'>
+
+              <div className='flex flex-col gap-4 place-items-center'>
+                <p className='font-medium'>Are You Sure To Delete ?</p>
+                <div className='flex flex-col gap-1 place-items-center'>
+                  <input type='text' value={cnfmName} className='p-2 px-3 bg-gray-200' placeholder='Enter Name To Proceed' onChange={(e)=>{
+                    setcnfmName(e.target.value);
+                  }}/>
+                  <p className=' text-xs text-red-400'>*Information is Case-Sensitive</p>
+                </div>
+              </div>
+
+              <div className='flex gap-10'>
+                <div className='bg-black text-white font-medium p-2 px-3 rounded-lg active:scale-105 transition-all' onClick={()=>{setdoAsk(false)}}>Cancel</div>
+
+                <div className='bg-black text-white font-medium p-2 px-3 rounded-lg active:scale-105 transition-all' onClick={confirmDelete}>Proceed</div>
+              </div>
+
+            </div>
+
+          </div>
+        )}
 
         <div className={`pb-6 flex justify-center place-items-center gap-44 transition-all ${update ? '':'hidden'}`}>
 
